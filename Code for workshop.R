a9c498bdb95cf2c785f5552c5cb48538f091276a
FAE-Sonata <heyu1993@gmail.com>
2023-03-21 16:00:48 -0400

    fix work directory to be flexible; fix zoom and legend on States by geographic area (last map in the code)

--- Code for workshop.R
+++ Code for workshop.R
@@ -1,20 +1,26 @@
-install.packages("choroplethr")
-install.packages("choroplethrMaps")
+## only run if needed
+# install.packages("choroplethr")
+# install.packages("choroplethrMaps")
 library(choroplethrMaps)
 library(choroplethr)
+## you may need to manually install gtable if one of the above fails: install.packages("gtable")
 
+# Getting the path of your current open file
+current_path<-rstudioapi::getActiveDocumentContext()$path
+setwd(dirname(current_path ))
+print( getwd() )
 
-newdata<-read.csv("F:/SUMMER STATS SEMINARS 2018/Geospatial - Data journalism/GEO CLASS/BEA.csv")
+newdata<-read.csv("BEA.csv")
 
 state_choropleth(newdata, title = "GDP by STATE",
                  legend = "US $")
 
-#or 
+# equivalent to (magrittr pipe)
 
 newdata%>%state_choropleth( title = "GDP by STATE",
                             legend = "US $")
 
-newdata<-read.csv("F:/SUMMER STATS SEMINARS 2018/Geospatial - Data journalism/GEO CLASS/CTY_MAP_WAGE.csv")
+newdata<-read.csv("CTY_MAP_WAGE.csv")
 newdata
 
 county_choropleth(newdata,title="Wages in 2016", legend="Sales $", num_colors=7)
@@ -25,7 +31,7 @@ county_choropleth(newdata,title="Wages in 2016", legend="Sales $", num_colors=7)
 
 library(choroplethr)
 
-newdata<-read.csv("F:/Geospatial/CTY_MAP_WAGE.csv")
+newdata<-read.csv("CTY_MAP_WAGE.csv")
 
 county_choropleth(newdata, state_zoom="illinois")
 
@@ -34,20 +40,19 @@ county_choropleth(newdata,
                   legend     = "Sales",
                   state_zoom = "maryland")
 
-
-
-BONUS MATERIAL  ;)
-
-
+# BONUS MATERIAL  ;) ------------------------------------------------------
 library(choroplethrMaps)
 data(county.regions)
 data(df_pop_county)
 library(dplyr)
+## NYC BOROUGHS ##
+
 nyc_county_names = c("kings", "bronx", "new york", "queens", "richmond")
 nyc_county_fips = county.regions %>% 
   filter(state.name == "new york" & county.name %in% nyc_county_names) %>%
   select(region)
 
+# borders mask the East River, but it should be clear which borough is which
 county_choropleth(df_pop_county,
                   title = "Population of Counties in New York City",
                   legend = "Population",
@@ -56,8 +61,8 @@ county_choropleth(df_pop_county,
                   )
 
 
-
-install.packages("leaflet")
+## BOSTON ##
+# install.packages("leaflet")
 library(leaflet)
 m <- leaflet() %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12)
 m %>% addTiles()
@@ -76,8 +81,6 @@ leaflet(data = quakes[1:20,]) %>% addTiles() %>%
 #working with GeoJSON files
 install.packages("geojsonio")
 library(geojsonio)
-getwd()
-setwd("F:/SUMMER STATS SEMINARS 2018/Leaflet")
 usstates <- geojsonio::geojson_read("gz_2010_us_040_00_500k.json",
                                     what = "sp")
 # Or use the rgdal equivalent:
@@ -85,10 +88,13 @@ usstates <- geojsonio::geojson_read("gz_2010_us_040_00_500k.json",
 
 pal <- colorNumeric("viridis", NULL)
 head(usstates)
+# 50 states + DC and Caribbean territories, only
 leaflet(usstates) %>%
+  setView(lng = -100, lat = 39, zoom=4) %>%
   addTiles() %>%
   addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1,
               fillColor = ~pal(log10(CENSUSAREA)),
               label = ~paste0(NAME, ": ", formatC(CENSUSAREA, big.mark = ","))) %>%
   addLegend(pal = pal, values = ~log10(CENSUSAREA), opacity = 1.0,
+            title="Log area (square miles)",
             labFormat = labelFormat(transform = function(x) round(10^x)))
